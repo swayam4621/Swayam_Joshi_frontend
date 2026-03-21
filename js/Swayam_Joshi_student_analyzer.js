a35629e6aae5cd1a6395b30a6a9210a4efeaec08
Swayam-Joshi <swayamjoshi13@gmail.com>
2026-03-22 02:24:07 +0530

    feat: adding student data structure

---
+++ js/Swayam_Joshi_student_analyzer.js
@@ -0,0 +1,37 @@
+//adding initial data
+
+const students = [
+    {
+        name: "Lalit",
+        marks: [
+            { subject: "Math", score: 78 },
+            { subject: "English", score: 82 },
+            { subject: "Science", score: 74 },
+            { subject: "History", score: 69 },
+            { subject: "Computer", score: 88 }
+        ],
+        attendance: 82
+    },
+    {
+        name: "Rahul",
+        marks: [
+            { subject: "Math", score: 90 },
+            { subject: "English", score: 85 },
+            { subject: "Science", score: 80 },
+            { subject: "History", score: 76 },
+            { subject: "Computer", score: 92 }
+        ],
+        attendance: 91
+    },
+    {
+        name: "Aman",
+        marks: [
+            { subject: "Math", score: 35 },
+            { subject: "English", score: 50 },
+            { subject: "Science", score: 45 },
+            { subject: "History", score: 40 },
+            { subject: "Computer", score: 55 }
+        ],
+        attendance: 80
+    }
+];
