1b4c216de968ea6031961343e73801c3d11fccb7
Swayam-Joshi <swayamjoshi13@gmail.com>
2026-04-12 00:59:11 +0530

    feat: Implement UserRepository with dummy data (in memory)

---
+++ Swayam_Joshi_java_training/session2/user-management-system/src/main/java/com/assignment/user_management_system/repository/UserRepository.java
@@ -0,0 +1,21 @@
+package com.assignment.user_management_system.repository;
+
+import com.assignment.user_management_system.model.User;
+import org.springframework.stereotype.Repository;
+import java.util.*;
+
+@Repository
+public class UserRepository {
+    private final List<User> users = new ArrayList<>(List.of(
+        new User(1L, "swayam joshi", "swayamjoshi13@gmail.com"),
+        new User(2L, "Akshat sharma", "akshat@gmail.com")
+    ));
+
+    public List<User> findAll() { return users; }
+    
+    public Optional<User> findById(Long id) {
+        return users.stream().filter(u -> u.getId().equals(id)).findFirst();
+    }
+
+    public void save(User user) { users.add(user); }
+}
