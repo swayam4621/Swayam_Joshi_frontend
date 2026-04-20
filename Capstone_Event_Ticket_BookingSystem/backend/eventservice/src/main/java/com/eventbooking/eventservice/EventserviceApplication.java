5c3059c76ed7251f628c1a1e28fcbff2817e0459
Swayam-Joshi <swayamjoshi13@gmail.com>
2026-04-21 01:47:44 +0530

    Initial commit and  Capstone project structure

---
+++ Capstone_Event_Ticket_BookingSystem/backend/eventservice/src/main/java/com/eventbooking/eventservice/EventserviceApplication.java
@@ -0,0 +1,13 @@
+package com.eventbooking.eventservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventserviceApplication.class, args);
+	}
+
+}
