355bf74e663610beebb1f2761bb98816fc3d5ded
Swayam-Joshi <swayamjoshi13@gmail.com>
2026-04-25 01:02:58 +0530

    feat: created EventRequest dto for data transfer

---
+++ Capstone_Event_Ticket_BookingSystem/backend/eventservice/src/main/java/com/eventbooking/eventservice/dto/EventRequest.java
@@ -0,0 +1,60 @@
+package com.eventbooking.eventservice.dto;
+
+import java.time.LocalDateTime;
+
+public class EventRequest {
+    private String title;
+    private String description;
+    private LocalDateTime eventDate;
+    private String location;
+    private double price;
+    private Integer totalTickets;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getEventDate() {
+        return eventDate;
+    }
+
+    public void setEventDate(LocalDateTime eventDate) {
+        this.eventDate = eventDate;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public Integer getTotalTickets() {
+        return totalTickets;
+    }
+
+    public void setTotalTickets(Integer totalTickets) {
+        this.totalTickets = totalTickets;
+    }
+}
