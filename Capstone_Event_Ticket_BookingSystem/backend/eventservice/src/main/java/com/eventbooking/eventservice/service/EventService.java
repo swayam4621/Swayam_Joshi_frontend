ab6aeeef66d00e8d5a57a7ebe577e3fb7a4ffa34
Swayam-Joshi <swayamjoshi13@gmail.com>
2026-04-25 01:09:31 +0530

    feat(event): implement event service business logic for Create and update operations

---
+++ Capstone_Event_Ticket_BookingSystem/backend/eventservice/src/main/java/com/eventbooking/eventservice/service/EventService.java
@@ -0,0 +1,58 @@
+package com.eventbooking.eventservice.service;
+
+import com.eventbooking.eventservice.dto.EventRequest;
+import com.eventbooking.eventservice.entity.Event;
+import com.eventbooking.eventservice.exception.EventNotFoundException;
+import com.eventbooking.eventservice.exception.UnauthorizedAccessException;
+import com.eventbooking.eventservice.repository.EventRepository;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class EventService {
+
+    private final EventRepository eventRepository;
+
+    public EventService(EventRepository eventRepository) {
+        this.eventRepository = eventRepository;
+    }
+
+    public Event createEvent(EventRequest request, String organizerEmail) {
+        Event event = new Event();
+        
+        event.setName(request.getTitle()); 
+        event.setDescription(request.getDescription());
+        event.setEventDateTime(request.getEventDate()); 
+        event.setVenue(request.getLocation()); 
+        event.setTicketPrice(request.getPrice()); 
+        event.setTotalSeats(request.getTotalTickets()); 
+        event.setAvailableSeats(request.getTotalTickets()); 
+        
+        event.setOrganizerEmail(organizerEmail); 
+
+        return eventRepository.save(event);
+    }
+
+    public Event updateEvent(Long eventId, EventRequest request, String organizerEmail) {
+        Event event = eventRepository.findById(eventId)
+                .orElseThrow(() -> new EventNotFoundException("Event with ID " + eventId + " could not be found."));
+
+        //prevents a NullPointerException if event.getOrganizerEmail() is  null
+        if (!organizerEmail.equals(event.getOrganizerEmail())) {
+            throw new UnauthorizedAccessException("Not authorized to update this event.");
+        }
+
+        event.setName(request.getTitle());
+        event.setDescription(request.getDescription());
+        event.setEventDateTime(request.getEventDate());
+        event.setVenue(request.getLocation());
+        event.setTicketPrice(request.getPrice());
+        event.setTotalSeats(request.getTotalTickets());
+
+        return eventRepository.save(event);
+    }
+
+    public List<Event> getEventsByOrganizer(String organizerEmail) {
+        return eventRepository.findByOrganizerEmail(organizerEmail);
+    }
+}
