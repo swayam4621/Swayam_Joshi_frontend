1f1f192117c20c679e6f803f451e4cf172802daf
Swayam-Joshi <swayamjoshi13@gmail.com>
2026-04-29 01:41:10 +0530

    feat(booking): created BookingRepository

---
+++ Capstone_Event_Ticket_BookingSystem/backend/eventservice/src/main/java/com/eventbooking/eventservice/repository/BookingRepository.java
@@ -0,0 +1,17 @@
+package com.eventbooking.eventservice.repository;
+
+import com.eventbooking.eventservice.entity.Booking;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface BookingRepository extends JpaRepository<Booking, Long> {
+    
+    //For showing a customer all the tickets they have bought
+    List<Booking> findByUserEmail(String userEmail);
+    
+    //For showing an organizer all the bookings for their events
+    List<Booking> findByEventId(Long eventId);
+}
