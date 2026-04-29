87ae3fd912c76b1a77dcdb79fe1d393ad766e1b6
Swayam-Joshi <swayamjoshi13@gmail.com>
2026-04-29 23:37:54 +0530

    feat(security): giving role: customer access to api/bookings/cancel

--- Capstone_Event_Ticket_BookingSystem/backend/eventservice/src/main/java/com/eventbooking/eventservice/config/SecurityConfig.java
+++ Capstone_Event_Ticket_BookingSystem/backend/eventservice/src/main/java/com/eventbooking/eventservice/config/SecurityConfig.java
@@ -43,7 +43,7 @@ public class SecurityConfig {
                 
                     .requestMatchers(HttpMethod.GET, "/api/events", "/api/events/{id}").permitAll()
 
-                .requestMatchers("/api/bookings/create", "/api/bookings/my-bookings").hasAnyAuthority("CUSTOMER", "ROLE_CUSTOMER")
+                .requestMatchers("/api/bookings/create", "/api/bookings/my-bookings", "/api/bookings/cancel/**").hasAnyAuthority("CUSTOMER", "ROLE_CUSTOMER")
                 .requestMatchers("/api/bookings/event/**").hasAnyAuthority("ORGANIZER", "ROLE_ORGANIZER")
 
                 .anyRequest().authenticated()
